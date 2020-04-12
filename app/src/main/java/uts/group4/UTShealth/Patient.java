f9e15c0c9480812b1c52035b72c5dd4c275dcc37
oattions <angelasunn98@gmail.com>
2020-04-12 18:53:02 +1000

    Created patient class and base fields

---
+++ app/src/main/java/uts/group4/UTShealth/Patient.java
@@ -0,0 +1,23 @@
+package uts.group4.UTShealth;
+
+/***************************************************************************************************
+ * A class which stores all the data of a patient
+ **************************************************************************************************/
+
+public class Patient {
+    private String patientID;
+
+    private String firstName;
+    private String lastName;
+
+    private String medicareNumber;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String postCode;
+
+}
