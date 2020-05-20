5f468980286d615e6d5d9e7bd08150c10d87d1a5
swizel03 <62579017+swizel03@users.noreply.github.com>
2020-05-20 21:04:19 +1000

    Added notes page

--- app/src/main/java/uts/group4/UTShealth/Prescription.java
+++ app/src/main/java/uts/group4/UTShealth/Prescription.java
@@ -1,13 +1,23 @@
 package uts.group4.UTShealth;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import maes.tech.intentanim.CustomIntent;
 
@@ -21,6 +31,10 @@ public class Prescription extends AppCompatActivity {
     private static String recipe;
     private static String medIns;
     private static String dispIns;
+    FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+    String userID = fAuth.getCurrentUser().getUid();
+    DocumentReference patNameRef = fStore.collection("Patients").document(userID);
 
 
     @Override
@@ -35,6 +49,25 @@ public class Prescription extends AppCompatActivity {
         medInsTf = findViewById(R.id.medInsTf);
         dispInsTf = findViewById(R.id.dispInsTf);
         doneBtn = findViewById(R.id.doneBtn);
+/*
+        patNameRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String patientFullName = documentSnapshot.getString("First Name") + " " + documentSnapshot.getString("Last Name");
+                    patNameTf.setText(patientFullName);
+                } else {
+                    Toast.makeText(Prescription.this, "Error: Patient name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Prescription.this, "Error: Patient name", Toast.LENGTH_SHORT).show();
+            }
+        });
+ */
 
         doneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
