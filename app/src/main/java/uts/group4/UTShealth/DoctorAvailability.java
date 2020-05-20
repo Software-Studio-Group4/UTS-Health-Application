24093febd21cabc1b7c90dbcf8dda2c3cd0e17ca
oattions <angelasunn98@gmail.com>
2020-05-21 07:15:13 +1000

    added recycler view for all days of the week
    to do :
    -let the doctor take time off

--- app/src/main/java/uts/group4/UTShealth/DoctorAvailability.java
+++ app/src/main/java/uts/group4/UTShealth/DoctorAvailability.java
@@ -34,6 +34,18 @@ public class DoctorAvailability extends AppCompatActivity {
     CollectionReference shiftRef = fStore.collection("Doctor").document(userID).collection("Shifts");
     private RecyclerView sunRecyclerView;
     private FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder> sunAdapter;
+    private RecyclerView monRecyclerView;
+    private FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder> monAdapter;
+    private RecyclerView tueRecyclerView;
+    private FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder> tueAdapter;
+    private RecyclerView wedRecyclerView;
+    private FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder> wedAdapter;
+    private RecyclerView thuRecyclerView;
+    private FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder> thuAdapter;
+    private RecyclerView friRecyclerView;
+    private FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder> friAdapter;
+    private RecyclerView satRecyclerView;
+    private FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder> satAdapter;
 
 
     @Override
@@ -41,24 +53,8 @@ public class DoctorAvailability extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doctoravailability_layout);
 
-        sunRecyclerView = findViewById(R.id.sunRecycler);
-        sunRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        Query sundayShiftsQuery = shiftRef.whereEqualTo("Day", "Sunday").orderBy("StartTime", Query.Direction.ASCENDING);
-        FirestoreRecyclerOptions<ShiftModel> options = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(sundayShiftsQuery, ShiftModel.class).build();
-        sunAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
-                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
-            }
-
-            @NonNull
-            @Override
-            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
-                return new ShiftViewHolder(view);
-            }
-        };
-        sunRecyclerView.setAdapter(sunAdapter);
+        /***********setting the recycler views**************/
+        initShifts();
     }
 
     /**********************************************************************************************
@@ -77,6 +73,12 @@ public class DoctorAvailability extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
         sunAdapter.startListening();
+        monAdapter.startListening();
+        tueAdapter.startListening();
+        wedAdapter.startListening();
+        thuAdapter.startListening();
+        friAdapter.startListening();
+        satAdapter.startListening();
     }
 
     @Override
@@ -85,6 +87,24 @@ public class DoctorAvailability extends AppCompatActivity {
 
         if (sunAdapter != null) {
             sunAdapter.stopListening();
+        }
+        if (tueAdapter != null) {
+            tueAdapter.stopListening();
+        }
+        if (wedAdapter != null) {
+            wedAdapter.stopListening();
+        }
+        if (thuAdapter != null) {
+            thuAdapter.stopListening();
+        }
+        if (friAdapter != null) {
+            friAdapter.stopListening();
+        }
+        if (satAdapter != null) {
+            satAdapter.stopListening();
+        }
+        if (monAdapter != null) {
+            monAdapter.stopListening();
         }
     }
 
@@ -101,6 +121,150 @@ public class DoctorAvailability extends AppCompatActivity {
         EditShiftFragment editShiftFragment = new EditShiftFragment();
         editShiftFragment.setArguments(bundle);
         editShiftFragment.show(getSupportFragmentManager(), "Edit Shift");
+    }
+    /**********************************************************************************************
+     * Initialise the shift recyclers (this is huge)
+     ************************************************************************************************/
+    public void initShifts(){
+        //SUNDAY
+        sunRecyclerView = findViewById(R.id.sunRecycler);
+        sunRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query sundayShiftsQuery = shiftRef.whereEqualTo("Day", "Sunday").orderBy("StartTime", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<ShiftModel> options = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(sundayShiftsQuery, ShiftModel.class).build();
+        sunAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
+                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
+            }
+
+            @NonNull
+            @Override
+            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
+                return new ShiftViewHolder(view);
+            }
+        };
+        sunRecyclerView.setAdapter(sunAdapter);
+
+        //MONDAY
+        monRecyclerView = findViewById(R.id.monRecycler);
+        monRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query mondayShiftsQuery = shiftRef.whereEqualTo("Day", "Monday").orderBy("StartTime", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<ShiftModel> optionsMon = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(mondayShiftsQuery, ShiftModel.class).build();
+        monAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(optionsMon) {
+            @Override
+            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
+                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
+            }
+
+            @NonNull
+            @Override
+            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
+                return new ShiftViewHolder(view);
+            }
+        };
+        monRecyclerView.setAdapter(monAdapter);
+
+        //TUESDAY
+        tueRecyclerView = findViewById(R.id.tueRecycler);
+        tueRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query tuesdayShiftsQuery = shiftRef.whereEqualTo("Day", "Tuesday").orderBy("StartTime", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<ShiftModel> tueOptions = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(tuesdayShiftsQuery, ShiftModel.class).build();
+        tueAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(tueOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
+                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
+            }
+
+            @NonNull
+            @Override
+            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
+                return new ShiftViewHolder(view);
+            }
+        };
+        tueRecyclerView.setAdapter(tueAdapter);
+
+        //WEDNESDAY
+        wedRecyclerView = findViewById(R.id.wedRecycler);
+        wedRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query wednesdayShiftsQuery = shiftRef.whereEqualTo("Day", "Wednesday").orderBy("StartTime", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<ShiftModel> wedOptions = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(wednesdayShiftsQuery, ShiftModel.class).build();
+        wedAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(wedOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
+                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
+            }
+
+            @NonNull
+            @Override
+            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
+                return new ShiftViewHolder(view);
+            }
+        };
+        wedRecyclerView.setAdapter(wedAdapter);
+
+        //THURSDAY
+        thuRecyclerView = findViewById(R.id.thuRecycler);
+        thuRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query thursdayShiftsQuery = shiftRef.whereEqualTo("Day", "Thursday").orderBy("StartTime", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<ShiftModel> thuOptions = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(thursdayShiftsQuery, ShiftModel.class).build();
+        thuAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(thuOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
+                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
+            }
+
+            @NonNull
+            @Override
+            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
+                return new ShiftViewHolder(view);
+            }
+        };
+        thuRecyclerView.setAdapter(thuAdapter);
+
+        //FRIDAY
+        friRecyclerView = findViewById(R.id.friRecycler);
+        friRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query fridayShiftsQuery = shiftRef.whereEqualTo("Day", "Friday").orderBy("StartTime", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<ShiftModel> friOptions = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(fridayShiftsQuery, ShiftModel.class).build();
+        friAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(friOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
+                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
+            }
+
+            @NonNull
+            @Override
+            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
+                return new ShiftViewHolder(view);
+            }
+        };
+        friRecyclerView.setAdapter(friAdapter);
+
+        //SATDAY
+        satRecyclerView = findViewById(R.id.satRecycler);
+        satRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query saturdayShiftsQuery = shiftRef.whereEqualTo("Day", "Saturday").orderBy("StartTime", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<ShiftModel> satOptions = new FirestoreRecyclerOptions.Builder<ShiftModel>().setQuery(saturdayShiftsQuery, ShiftModel.class).build();
+        satAdapter = new FirestoreRecyclerAdapter<ShiftModel, ShiftViewHolder>(satOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ShiftViewHolder shiftViewHolder, int position, @NonNull ShiftModel shiftModel) {
+                shiftViewHolder.setShiftData(shiftModel.getStartTime(), shiftModel.getEndTime(), shiftModel.getDay(), getSnapshots().getSnapshot(position).getId());
+            }
+
+            @NonNull
+            @Override
+            public ShiftViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shift, parent, false);
+                return new ShiftViewHolder(view);
+            }
+        };
+        satRecyclerView.setAdapter(satAdapter);
     }
 
     /**********************************************************************************************
@@ -132,7 +296,7 @@ public class DoctorAvailability extends AppCompatActivity {
             block.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(DoctorAvailability.this, "clicked on the shift", Toast.LENGTH_SHORT).show();
+                    
                     editShift(bundle);
                 }
             });
