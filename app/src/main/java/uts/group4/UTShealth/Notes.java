cbf4b4b043990e14b62cb0880b6dfa5d2456c741
swizel03 <62579017+swizel03@users.noreply.github.com>
2020-05-25 18:29:23 +1000

    Test code to put notes as an extra field in appointment collection.

--- app/src/main/java/uts/group4/UTShealth/Notes.java
+++ app/src/main/java/uts/group4/UTShealth/Notes.java
@@ -6,8 +6,23 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import maes.tech.intentanim.CustomIntent;
 
@@ -15,6 +30,8 @@ public class Notes extends AppCompatActivity {
 
     EditText notesTf;
     Button sendBtn, prescriptionBtn;
+    FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +45,43 @@ public class Notes extends AppCompatActivity {
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String notes = notesTf.getText().toString().trim();
+                final String notes = notesTf.getText().toString().trim();
                 if (TextUtils.isEmpty(notes)) {
                     notesTf.setError("Cannot have Empty Field");
                     return;
                 }
+                Bundle extras = getIntent().getExtras();
+                String chatCode = extras.getString("Chatroomcode");
+                fStore.collection("Appointment")
+                        .whereEqualTo("ChatCode", chatCode)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        String id = document.getId();
+                                        DocumentReference documentReference = fStore.collection("Appointment").document(id);
+                                        Map<String, Object> notesData = new HashMap<>(); //
+                                        notesData.put("Notes", notes);
+                                        documentReference.set(notesData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(Notes.this, "Success", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(Notes.this, "Error", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
+                                } else {
+                                    Toast.makeText(Notes.this, "Can't retrieve document", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                 startActivity(new Intent(getApplicationContext(), PatientDashboard.class));
                 CustomIntent.customType(Notes.this, "fadein-to-fadeout");
             }
