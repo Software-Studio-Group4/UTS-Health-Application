9e868355bc89533b0c1d8894f422129ebac315a6
swizel03 <62579017+swizel03@users.noreply.github.com>
2020-05-27 21:30:55 +1000

    Make a pdf and send it to an email. The email is currently mine.

--- app/src/main/java/uts/group4/UTShealth/Notes.java
+++ app/src/main/java/uts/group4/UTShealth/Notes.java
@@ -195,8 +195,6 @@ public class Notes extends AppCompatActivity implements Runnable {
         mShareIntent = new Intent();
         mShareIntent.setAction(Intent.ACTION_SEND);
         mShareIntent.setType("application/pdf");
-        String[] to = {"leesandra.aus@gmail.com"};
-        mShareIntent.putExtra(Intent.EXTRA_EMAIL, to);
         mShareIntent.putExtra(Intent.EXTRA_SUBJECT, "Here is a PDF from UTS Health Application");
         // Attach the PDf as a Uri.
         mShareIntent.putExtra(Intent.EXTRA_STREAM, uri);
