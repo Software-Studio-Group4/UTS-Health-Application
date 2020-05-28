c3f8ad24ef255f5865557112dcf9230d2962255c
swizel03 <62579017+swizel03@users.noreply.github.com>
2020-05-28 15:39:47 +1000

    Make a pdf and send it to an email. The email is currently mine.

--- app/src/main/java/uts/group4/UTShealth/Notes.java
+++ app/src/main/java/uts/group4/UTShealth/Notes.java
@@ -1,15 +1,7 @@
 package uts.group4.UTShealth;
 
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.pdf.PdfDocument;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.print.PrintAttributes;
-import android.print.pdf.PrintedPdfDocument;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +10,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -30,24 +21,18 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 import maes.tech.intentanim.CustomIntent;
 
-public class Notes extends AppCompatActivity implements Runnable {
+public class Notes extends AppCompatActivity{
 
     EditText notesTf;
-    Button saveBtn, sendBtn, doneBtn, backBtn;
+    Button saveBtn, backBtn;
     FirebaseAuth fAuth = FirebaseAuth.getInstance();
     FirebaseFirestore fStore = FirebaseFirestore.getInstance();
     String userID = fAuth.getCurrentUser().getUid();
-    private Intent mShareIntent;
-    private OutputStream os;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +40,6 @@ public class Notes extends AppCompatActivity implements Runnable {
         setContentView(R.layout.activity_notes);
         notesTf = findViewById(R.id.notesTf1);
         saveBtn = findViewById(R.id.saveBtn);
-        sendBtn = findViewById(R.id.sendBtn);
-        doneBtn = findViewById(R.id.doneBtn);
         backBtn = findViewById(R.id.backBtn);
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
@@ -97,15 +80,10 @@ public class Notes extends AppCompatActivity implements Runnable {
                                 } else {
                                     Toast.makeText(Notes.this, "Can't retrieve document", Toast.LENGTH_SHORT).show();
                                 }
+                                startActivity(new Intent(getApplicationContext(), Confirmation.class));
+                                CustomIntent.customType(Notes.this, "fadein-to-fadeout");
                             }
                         });
-            }
-        });
-        doneBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), StaffDashboard.class));
-                CustomIntent.customType(Notes.this, "fadein-to-fadeout");
             }
         });
 
@@ -116,90 +94,7 @@ public class Notes extends AppCompatActivity implements Runnable {
             }
         });
     }
-    public void makeAndSharePDF(View buttonSource) {
-        new Thread(this).start();
-    }
 
-    public void run() {
-
-        // Create a new PDF document in memory
-        // We want it to be printable, so add PrintAttributes
-        PrintAttributes printAttrs = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            printAttrs = new PrintAttributes.Builder().
-                    setColorMode(PrintAttributes.COLOR_MODE_COLOR).
-                    setMediaSize(PrintAttributes.MediaSize.NA_LETTER).
-                    setResolution(new PrintAttributes.Resolution("zooey", PRINT_SERVICE, 300, 300)).
-                    setMinMargins(PrintAttributes.Margins.NO_MARGINS).
-                    build();
-        }
-        PdfDocument document = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            document = new PrintedPdfDocument(this, printAttrs);
-        }
-
-        // crate a page description
-        PdfDocument.PageInfo pageInfo = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            pageInfo = new PdfDocument.PageInfo.Builder(300, 300, 1).create();
-        }
-
-        // create a new page from the PageInfo
-        PdfDocument.Page page = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            page = document.startPage(pageInfo);
-        }
-
-        // test to create something in the page
-        Canvas canvas = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-            canvas = page.getCanvas();
-        }
-
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-
-        canvas.drawCircle(50, 50, 30, paint);
-
-        // do final processing of the page
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            document.finishPage(page);
-        }
-
-
-        // Write the PDF document to a file
-        try {
-            File pdfDirPath = new File(getFilesDir(), "pdfs");
-            boolean wasSuccessful = pdfDirPath.mkdirs();
-            if (!wasSuccessful) {
-                System.out.println("was not successful.");
-            }
-            File file = new File(pdfDirPath, "pdfsend.pdf");
-            Uri contentUri = FileProvider.getUriForFile(this, "uts.group4.UTShealth.fileprovider", file);
-            os = new FileOutputStream(file);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                document.writeTo(os);
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                document.close();
-            }
-            os.close();
-
-            shareDocument(contentUri);
-        } catch (IOException e) {
-            throw new RuntimeException("Error generating file", e);
-        }
-    }
-
-    private void shareDocument(Uri uri) {
-        mShareIntent = new Intent();
-        mShareIntent.setAction(Intent.ACTION_SEND);
-        mShareIntent.setType("application/pdf");
-        mShareIntent.putExtra(Intent.EXTRA_SUBJECT, "Here is a PDF from UTS Health Application");
-        // Attach the PDf as a Uri.
-        mShareIntent.putExtra(Intent.EXTRA_STREAM, uri);
-        startActivity(Intent.createChooser(mShareIntent, "Send email..."));
-    }
 
 }
 
