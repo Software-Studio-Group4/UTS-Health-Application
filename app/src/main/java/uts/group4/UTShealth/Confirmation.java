7a0ca4d7706989ecfe99f1b9ad085db986b9c7ef
oattions <angelasunn98@gmail.com>
2020-05-29 18:03:47 +1000

    -resolved merge conflict with leesandra's push. Confirmed working- text is written to the pdf that is sent. -fixed a bug where pressing the confirm button threw a null pointer exception.

--- app/src/main/java/uts/group4/UTShealth/Confirmation.java
+++ app/src/main/java/uts/group4/UTShealth/Confirmation.java
@@ -91,9 +91,9 @@ public class Confirmation extends AppCompatActivity implements Runnable  {
         String chatCode = extras.getString("chatroomcode1");
         String stbmps = extras.getString("Bitmap");
         Bitmap bits = StringToBitMap(stbmps);
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bits,595, 842, false);
+       // Bitmap scaledBitmap = Bitmap.createScaledBitmap(bits,595, 842, false);
 
-        scaledBitmap.prepareToDraw();
+        //scaledBitmap.prepareToDraw();
 
         // test to create something in the page
         Canvas canvas = null;
